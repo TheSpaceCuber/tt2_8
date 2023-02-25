6c59d1fdaf733c53aadcec2d426e2437c4fcf749
Zhong Yi <zycrystal1201@gmail.com>
2023-02-25 13:45:17 +0800

    View Policy

--- frontend/tt2_8/src/pages/ViewPolicy.jsx
+++ frontend/tt2_8/src/pages/ViewPolicy.jsx
@@ -1,12 +1,35 @@
 import Button from 'react-bootstrap/Button';
 import Navbar from '../Components/Navbar.jsx';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 
-function ViewPolicy() {
+
+
+  function ViewPolicy() {
     return (
       <div>
-        <Navbar></Navbar>
-        <Button>Something</Button>
-        
+      <Navbar></Navbar>
+        <Container>
+      <Row className='mt-3'>
+      <h2>Your Policies</h2>
+      </Row>       
+      <Row>
+      <Col lg="3">
+        <Card>
+        <Card.Body>
+          <Card.Title><TextSnippetIcon/> Policy ID</Card.Title>    
+          <Card.Text>
+            Policy details here
+          </Card.Text>
+          <Card.Link href="#">Make Claims</Card.Link>
+        </Card.Body>
+      </Card>
+      </Col>
+      </Row>
+      </Container>
       </div>
     );
   }
