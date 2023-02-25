333cd770787e063fdef94c7e2895ec9574f339a9
jiemin-rgb <80969356+jiemin-rgb@users.noreply.github.com>
2023-02-25 16:26:10 +0800

    added authentication (#14)

--- backend/routes/policy.router.js
+++ backend/routes/policy.router.js
@@ -1,11 +1,12 @@
 import express from "express";
-import{
+import {
     createPolicy,
 } from "../controllers/policy.controller.js";
+import { generateAccessToken, authenticateToken } from '../jwt.js';
 
 const policyRouter = express.Router();
 
-policyRouter.post('/add-policy/', createPolicy);
+policyRouter.post('/add-policy/', authenticateToken, createPolicy);
 
 
 export default policyRouter;
