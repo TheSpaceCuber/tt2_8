ccb45a8df618c21993f6b12605a0441a24a189ec
bewx77 <beth.engwx@gmail.com>
2023-02-25 13:33:55 +0800

    Added routers for claims

--- backend/routes/claims.router.js
+++ backend/routes/claims.router.js
@@ -1,21 +1,24 @@
 import express from "express";
 import{
-    createUser,
-    getUserById,
-    updateUserPassword,
-    updateUserCreditAdd,
-    updateUserCreditMinus,
-    deleteUser
+    createInsuranceClaim,
+    getInsuranceClaim,
+    editInsuranceClaim,
+    deleteInsuranceClaim
 } from "../controllers/claims.controller.js";
-import { verifyUser, adminOnly } from "../middleware/authUser.middleware.js";
+// import { verifyUser, adminOnly } from "../middleware/authUser.middleware.js";
 
-const router = express.Router();
+const claimsRouter = express.Router();
 
-router.post('/users', createUser);
-router.get('/users/uuid/:id', verifyUser, getUserById);
-router.patch('/users/updatepw/:id', verifyUser, updateUserPassword);
-router.patch('/users/addcred/:id', verifyUser, updateUserCreditAdd);
-router.patch('/users/minuscred/:id', verifyUser, updateUserCreditMinus);
-router.delete('/users/delete/:id', verifyUser, deleteUser);
+//(1) Get all claims of employee id
+claimsRouter.get('/claims/:id', getInsuranceClaim);
 
-export default router;
+//(2) Create claim
+claimsRouter.post('/add-claim', createInsuranceClaim);
+
+//(3) Edit claim
+claimsRouter.patch('/edit-claim/:id', editInsuranceClaim);
+
+//(4) Delete claim
+claimsRouter.delete('/delete-claim/:id', deleteInsuranceClaim);
+
+export default claimsRouter;
