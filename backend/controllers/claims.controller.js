268d5c0bf51f3e375fc4dca8c6b842d134adcc83
Royce <royce963@gmail.com>
2023-02-25 16:04:20 +0800

    Feat/claims beth (#13)
    * Delete and patch request for claims
    
    * Edit status code
    
    * fix bugs
    
    * fix
    
    ---------
    
    Co-authored-by: bewx77 <112966395+bewx77@users.noreply.github.com>

--- backend/controllers/claims.controller.js
+++ backend/controllers/claims.controller.js
@@ -4,11 +4,11 @@ import InsurancePolicies from '../models/policy.model.js';
 
 import { Sequelize, DataTypes, Op } from "sequelize"
 
-export const createInsuranceClaim = async(req, res) => {
-    const { insuranceId, firstName, lastName, expenseDate,amount,purpose, followUp, previousClaimId} = req.body;
+export const createInsuranceClaim = async (req, res) => {
+    const { insuranceId, firstName, lastName, expenseDate, amount, purpose, followUp, previousClaimId } = req.body;
     try {
         const insuranceclaim = await InsuranceClaims.create({
-            
+
             insuranceId: insuranceId,
             firstName: firstName,
             lastName: lastName,
@@ -27,7 +27,7 @@ export const createInsuranceClaim = async(req, res) => {
     }
 }
 
-export const getAllClaimsForEmployee = async(req, res) => {
+export const getAllClaimsForEmployee = async (req, res) => {
     const relevantPolicies = await InsurancePolicies.findAll({
         where: {
             employeeId: req.params.employeeId
@@ -47,10 +47,10 @@ export const getAllClaimsForEmployee = async(req, res) => {
                 insuranceId: insuranceIds
             }
         });
-        if (relevantClaims.length === 0) return res.status(404).json({ msg: "Employee Claims Not Found"});
+        if (relevantClaims.length === 0) return res.status(404).json({ msg: "Employee Claims Not Found" });
         res.status(200).json(relevantClaims);
     } catch (error) {
-        res.status(500).json({msg: error.message});
+        res.status(500).json({ msg: error.message });
     }
 }
 
@@ -60,42 +60,7 @@ export const getAllClaimsForEmployee = async(req, res) => {
 // }
 
 
-export const getAllClaimsForEmployee = async(req, res) => {
-    const relevantPolicies = await InsurancePolicies.findAll({
-        where: {
-            employeeId: req.params.employeeId
-        }
-    });
-    console.log(relevantPolicies);
-    if (!relevantPolicies) return res.status(404).json({ msg: "Employee Policies Not Found" });
-
-    const insuranceIds = []
-    for (const insurancepolicies of relevantPolicies) {
-        insuranceIds.push(insurancepolicies.insuranceId);
-    }
-
-    try {
-        const relevantClaims = await InsuranceClaims.findAll({
-            where: {
-                insuranceId: insuranceIds
-            }
-        });
-        if (relevantClaims.length === 0) return res.status(404).json({ msg: "Employee Claims Not Found"});
-        res.status(200).json(relevantClaims);
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-}
-
-
-
-
-// export const editInsuranceClaim = async(req, res) => {
-//     const {}
-// }
-
-
-export const deleteInsuranceClaim = async(req, res) => {
+export const deleteInsuranceClaim = async (req, res) => {
 
     const insuranceclaim = await InsuranceClaims.findOne({
         where: {
@@ -118,8 +83,8 @@ export const deleteInsuranceClaim = async(req, res) => {
     }
 }
 
-export const editInsuranceClaim = async(req, res) => {
-    let {expenseDate,amount,purpose, followUp, previousClaimId} = req.body;
+export const editInsuranceClaim = async (req, res) => {
+    let { expenseDate, amount, purpose, followUp, previousClaimId } = req.body;
 
     const insuranceclaim = await InsuranceClaims.findOne({
         where: {
@@ -139,7 +104,7 @@ export const editInsuranceClaim = async(req, res) => {
     //check if claim is in "Pending" status to be deleted
     if (insuranceclaim.status == "Approved") return res.status(500).json({ msg: "Insurance Claim cannot be editted. Can only be editted if in Pending or Rejected Status" });
     try {
-        const insuranceclaim = await InsuranceClaims.update({  
+        const insuranceclaim = await InsuranceClaims.update({
             expenseDate: expenseDate,
             amount: amount,
             purpose: purpose,
@@ -147,7 +112,7 @@ export const editInsuranceClaim = async(req, res) => {
             previousClaimId: previousClaimId,
             lastEditedClaimDate: lastEditedClaimDate
 
-        }, { where: {claimId: req.params.id}, fields: ["expenseDate", "amount", "purpose", "followUp", "previousClaimId", "lastEditedClaimDate"]});
+        }, { where: { claimId: req.params.id }, fields: ["expenseDate", "amount", "purpose", "followUp", "previousClaimId", "lastEditedClaimDate"] });
         res.status(200).json({ msg: `Insurance Claim ${req.params.id} has been successfully edited.` });
     } catch (error) {
         res.status(500).json({ msg: error.message });
