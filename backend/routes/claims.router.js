7a32c80da57419bf9f154de73622cc13ab43ad75
bewx77 <112966395+bewx77@users.noreply.github.com>
2023-02-25 15:54:24 +0800

    fix bugs

--- backend/routes/claims.router.js
+++ backend/routes/claims.router.js
@@ -1,7 +1,7 @@
 import express from "express";
 import{
     createInsuranceClaim,
-    getAllClaimsForEmployee
+    getAllClaimsForEmployee,
     // getInsuranceClaim,
     editInsuranceClaim,
     deleteInsuranceClaim
