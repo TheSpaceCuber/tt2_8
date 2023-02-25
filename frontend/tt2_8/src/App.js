57f39e21a1475f090387eb1f5fb7df9803d757b7
onthewaysia <atsqw001@gmail.com>
2023-02-25 11:46:50 +0800

    preliminary routing done

--- frontend/tt2_8/src/App.js
+++ frontend/tt2_8/src/App.js
@@ -1,18 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import CreateClaim from "./pages/CreateClaim";
+import ViewClaimDetails from "./pages/ViewClaimDetails";
+import ViewClaims from "./pages/ViewClaims";
 import ViewPolicy from "./pages/ViewPolicy";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<ViewPolicy />} />
-        <Route path="/" element={<ViewPolicy />} />
-        <Route path="/" element={<ViewPolicy />} />
-        <Route path="/" element={<ViewPolicy />} />
-        <Route path="/" element={<ViewPolicy />} />
+        <Route path="/create_claim" element={<CreateClaim />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/view_claims_details" element={<ViewClaimDetails />} />
+        <Route path="/view_claims_details" element={<ViewClaims />} />
+        <Route path="/view_policy" element={<ViewPolicy />} />
       </Routes>
     </>
   );
