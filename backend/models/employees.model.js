0b91af5399a25fa9d2ff9b854243bb0416778b3f
Royce Ho <royce963@gmail.com>
2023-02-25 14:52:42 +0800

    Co-authored-by: victorwongyd <victorwongyd@users.noreply.github.com> Co-authored-by: bewx77 <bewx77@users.noreply.github.com> Co-authored-by: jiemin-rgb <jiemin-rgb@users.noreply.github.com> Co-authored-by: yuxuanneo <yuxuanneo@users.noreply.github.com>

--- backend/models/employees.model.js
+++ backend/models/employees.model.js
@@ -36,14 +36,6 @@ const Employees = db.define('employees', {
             len: [1, 100]
         }
     },
-    middleName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-            len: [1, 100]
-        }
-    },
     lastName: {
         type: DataTypes.STRING,
         allowNull: false,
